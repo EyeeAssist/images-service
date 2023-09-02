6543eade96838b551206c773aa870c97f416d277
XantX <hp_xantx@hotmail.com>
2023-09-02 18:45:37 -0500

    [feature] Implementacion de autenticacion

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-from PIL import Image
-from fastapi import FastAPI, UploadFile
-from io import BytesIO
-from service.image_service import process_image
+from fastapi import FastAPI
+from auth.auth_controller import router as AuthRouter
+from service.image_controller import router as ImageRouter
+
 
 app = FastAPI()
+
+app.include_router(AuthRouter)
+app.include_router(ImageRouter)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.post("/caption")
-async def generate_caption(file: UploadFile):
-    try:
-        image_data = await file.read()
-        image_pil = Image.open(BytesIO(image_data))
-        result = process_image(image_pil.convert("RGB"))
-        return {"message": result}
-    except print(0):
-        pass
