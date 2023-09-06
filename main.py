3b713546fe6a00b078e13d6de1baba1e4f0e605d
XantX <hp_xantx@hotmail.com>
2023-09-05 19:52:26 -0500

    [feature] Implementacion de cors

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from auth.auth_controller import router as AuthRouter
 from service.image_controller import router as ImageRouter
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(AuthRouter)
 app.include_router(ImageRouter)
